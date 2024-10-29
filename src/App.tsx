56261273005adee5e7b59b14f68a51ea850ddc90
Tharanishwaran <tharanishwaran.ta@gmail.com>
2024-10-30 00:14:50 +0530

    Added-Projects-Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 // src/App.tsx
 import React from 'react';
 import HomePage from './components/Home';
+import Projects from './components/Projects';
+import About from './components/About';
+
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <HomePage />
+            <Projects />
+            <About />
         </div>
     );
 };
